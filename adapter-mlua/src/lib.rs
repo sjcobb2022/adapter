9af4553e47d9e4d902d4c9066d9ee7967e5abc53
Samuel Cobb <sjcobb2003@gmail.com>
2024-07-15 00:55:47 +0100

    develop flake

--- adapter-mlua/src/lib.rs
+++ adapter-mlua/src/lib.rs
@@ -1,7 +1,37 @@
+use adapter::Adapter;
+
+use mlua::prelude::*;
+
+struct MLuaAdapter {
+    lua: Lua,
+}
+
+impl MLuaAdapter {
+    fn new() -> Self {
+        MLuaAdapter { lua: Lua::new() }
+    }
+}
+
+impl<'a, 'b, Input, Output, Identifier> Adapter<'b, Input, Output, Identifier, mlua::Error>
+    for MLuaAdapter
+where
+    // TODO: Convert to generic associated impl type when stable.
+    Identifier: AsRef<str>,
+{
+    fn call(&'b mut self, identifier: Identifier, input: Input) -> Result<Output, mlua::Error> {
+        self.lua.load(identifier).call::<Input, Output>(input)
+        // self.lua.
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn it_works() {
+        // let mut adapter = MLuaAdapter::new();
+        //
+        // let out: () = adapter.call("print", "Hello World!").unwrap();
+    }
 }
