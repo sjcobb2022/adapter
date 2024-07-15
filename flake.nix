a7d5e9b8eeb9b571ee784773f2c58923396be7db
Samuel Cobb <sjcobb2003@gmail.com>
2024-07-15 17:34:11 +0100

    initial flake nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,29 @@
 {
   description = "Simple devshell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default =
-          pkgs.mkShell { buildInputs = with pkgs; [ 
-            cargo 
-            rustc 
-            lua5_4_compat
-            pkg-config
-            rust-analyzer 
-            clippy 
-            rustfmt 
-            vscode-extensions.vadimcn.vscode-lldb.adapter
-          ]; };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          lua5_4_compat
+          pkg-config
+          rust-analyzer
+          clippy
+          rustfmt
+          vscode-extensions.vadimcn.vscode-lldb.adapter
+        ];
+      };
+
+      formatter = pkgs.alejandra;
+    });
 }
